ce81ba2620443cf0e5c0a6952503d04085b1e214
Skeen <skeen@cs.au.dk>
2014-04-08 14:36:11 +0200

    Forgot to cleanup skin.cpp

--- src/skin.cpp
+++ src/skin.cpp
@@ -7,49 +7,47 @@ void generateSkins(int layerNr, SliceVolumeStorage& storage, int extrusionWidth,
 {
     SliceLayer* layer = &storage.layers[layerNr];
 
-    for(unsigned int partNr=0; partNr<layer->parts.size(); partNr++)
+    for(SliceLayerPart& part : layer->parts)
     {
-        SliceLayerPart* part = &layer->parts[partNr];
-        
-        Polygons upskin = part->insets[part->insets.size() - 1].offset(-extrusionWidth/2);
+        Polygons upskin = part.insets[part.insets.size() - 1].offset(-extrusionWidth/2);
         Polygons downskin = upskin;
         
-        if (part->insets.size() > 1)
+        if (part.insets.size() > 1)
         {
             //Add thin wall filling by taking the area between the insets.
-            Polygons thinWalls = part->insets[0].offset(-extrusionWidth / 2 - extrusionWidth * infillOverlap / 100).difference(part->insets[1].offset(extrusionWidth * 6 / 10));
+            Polygons thinWalls = part.insets[0].offset(-extrusionWidth / 2 - extrusionWidth * infillOverlap / 100).difference(part.insets[1].offset(extrusionWidth * 6 / 10));
             upskin.add(thinWalls);
             downskin.add(thinWalls);
         }
         if (int(layerNr - downSkinCount) >= 0)
         {
             SliceLayer* layer2 = &storage.layers[layerNr - downSkinCount];
-            for(unsigned int partNr2=0; partNr2<layer2->parts.size(); partNr2++)
+            for(SliceLayerPart& comp : layer2->parts)
             {
-                if (part->boundaryBox.hit(layer2->parts[partNr2].boundaryBox))
-                    downskin = downskin.difference(layer2->parts[partNr2].insets[layer2->parts[partNr2].insets.size() - 1]);
+                if (part.boundaryBox.hit(comp.boundaryBox))
+                    downskin = downskin.difference(comp.insets[comp.insets.size() - 1]);
             }
         }
         if (int(layerNr + upSkinCount) < (int)storage.layers.size())
         {
             SliceLayer* layer2 = &storage.layers[layerNr + upSkinCount];
-            for(unsigned int partNr2=0; partNr2<layer2->parts.size(); partNr2++)
+            for(SliceLayerPart& comp : layer2->parts)
             {
-                if (part->boundaryBox.hit(layer2->parts[partNr2].boundaryBox))
-                    upskin = upskin.difference(layer2->parts[partNr2].insets[layer2->parts[partNr2].insets.size() - 1]);
+                if (part.boundaryBox.hit(comp.boundaryBox))
+                    upskin = upskin.difference(comp.insets[comp.insets.size() - 1]);
             }
         }
         
-        part->skinOutline = upskin.unionPolygons(downskin);
+        part.skinOutline = upskin.unionPolygons(downskin);
 
         double minAreaSize = (2 * M_PI * INT2MM(extrusionWidth) * INT2MM(extrusionWidth)) * 0.3;
-        part->skinOutline.erase(std::remove_if(part->skinOutline.begin(), part->skinOutline.end(),
+        part.skinOutline.erase(std::remove_if(part.skinOutline.begin(), part.skinOutline.end(),
                 [minAreaSize](PolygonRef r)
                 {
                     // Only create an up/down skin if the area is large enough. So you do not create tiny blobs of "trying to fill"
                     double area = INT2MM(INT2MM(fabs(r.area())));
                     return (area < minAreaSize);
-                }), part->skinOutline.end());
+                }), part.skinOutline.end());
     }
 }
 
@@ -57,26 +55,24 @@ void generateSparse(int layerNr, SliceVolumeStorage& storage, int extrusionWidth
 {
     SliceLayer* layer = &storage.layers[layerNr];
 
-    for(unsigned int partNr=0; partNr<layer->parts.size(); partNr++)
+    for(SliceLayerPart& part : layer->parts)
     {
-        SliceLayerPart* part = &layer->parts[partNr];
-
-        Polygons sparse = part->insets[part->insets.size() - 1].offset(-extrusionWidth/2);
+        Polygons sparse = part.insets[part.insets.size() - 1].offset(-extrusionWidth/2);
         Polygons downskin = sparse;
         Polygons upskin = sparse;
         
         if (int(layerNr - downSkinCount) >= 0)
         {
             SliceLayer* layer2 = &storage.layers[layerNr - downSkinCount];
-            for(unsigned int partNr2=0; partNr2<layer2->parts.size(); partNr2++)
+            for(SliceLayerPart& comp : layer2->parts)
             {
-                if (part->boundaryBox.hit(layer2->parts[partNr2].boundaryBox))
+                if (part.boundaryBox.hit(comp.boundaryBox))
                 {
-                    if (layer2->parts[partNr2].insets.size() > 1)
+                    if (comp.insets.size() > 1)
                     {
-                        downskin = downskin.difference(layer2->parts[partNr2].insets[layer2->parts[partNr2].insets.size() - 2]);
+                        downskin = downskin.difference(comp.insets[comp.insets.size() - 2]);
                     }else{
-                        downskin = downskin.difference(layer2->parts[partNr2].insets[layer2->parts[partNr2].insets.size() - 1]);
+                        downskin = downskin.difference(comp.insets[comp.insets.size() - 1]);
                     }
                 }
             }
@@ -84,15 +80,15 @@ void generateSparse(int layerNr, SliceVolumeStorage& storage, int extrusionWidth
         if (int(layerNr + upSkinCount) < (int)storage.layers.size())
         {
             SliceLayer* layer2 = &storage.layers[layerNr + upSkinCount];
-            for(unsigned int partNr2=0; partNr2<layer2->parts.size(); partNr2++)
+            for(SliceLayerPart& comp : layer2->parts)
             {
-                if (part->boundaryBox.hit(layer2->parts[partNr2].boundaryBox))
+                if (part.boundaryBox.hit(comp.boundaryBox))
                 {
-                    if (layer2->parts[partNr2].insets.size() > 1)
+                    if (comp.insets.size() > 1)
                     {
-                        upskin = upskin.difference(layer2->parts[partNr2].insets[layer2->parts[partNr2].insets.size() - 2]);
+                        upskin = upskin.difference(comp.insets[comp.insets.size() - 2]);
                     }else{
-                        upskin = upskin.difference(layer2->parts[partNr2].insets[layer2->parts[partNr2].insets.size() - 1]);
+                        upskin = upskin.difference(comp.insets[comp.insets.size() - 1]);
                     }
                 }
             }
@@ -109,6 +105,6 @@ void generateSparse(int layerNr, SliceVolumeStorage& storage, int extrusionWidth
                     return (area < minAreaSize);
                 }), result.end());
 
-        part->sparseOutline = sparse.difference(result);
+        part.sparseOutline = sparse.difference(result);
     }
 }
