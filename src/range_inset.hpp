fa1c78e835293e5a62bd42706529ded55298025f
Skeen <skeen@cs.au.dk>
2014-04-08 11:41:34 +0200

    Made PolygonRef and Polygon iterateable Split fffProcessor and multivolumes into h/cpp files.

---
+++ src/range_inset.hpp
@@ -0,0 +1,24 @@
+#ifndef _RANGE_INSET
+#define _RANGE_INSET
+
+template <typename Collection>
+struct RangeInset
+{
+    RangeInset(Collection& c, int start_inset = 0, int stop_inset = 0) : c(c), start_inset(start_inset), stop_inset(stop_inset) {}
+
+    auto begin() const { return c.begin() + start_inset; }
+    auto end() const { return c.end() - stop_inset; }
+
+    Collection& c;
+    int start_inset;
+    int stop_inset;
+};
+
+template<typename Collection>
+auto make_range_inset(Collection& c, int start_inset = 0, int stop_inset = 0)
+{
+    return RangeInset<Collection>(c, start_inset, stop_inset);
+}
+
+#endif //_RANGE_INSET
+
